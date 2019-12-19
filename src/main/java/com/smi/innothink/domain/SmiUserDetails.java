d9c7d020ea3997eab30b2eedd1dd26d99ccb625b
JOHNTECH <ajohnpaulantony@gmail.com>
2019-12-19 18:30:54 +0530

    Update SmiUserDetails.java

--- src/main/java/com/smi/innothink/domain/SmiUserDetails.java
+++ src/main/java/com/smi/innothink/domain/SmiUserDetails.java
@@ -3,9 +3,6 @@ package com.smi.innothink.domain;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class SmiUserDetails{ /*extends Trainers implements UserDetails {
 
